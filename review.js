c14f39561504b8b082fab40b3f63512d51ed3536
Desiree Quade <nekodez@gmail.com>
2021-12-06 11:19:15 -0600

    index.html and review.js

---
+++ review.js
@@ -0,0 +1,22 @@
+/*
+Jargon!!
+
+. - dot
+() - parentheses, parens
+[] - square brackiets, square braces
+{} - curly braces, curlies
+< - less than, bird, angle brackets
+> -  greater than, gator, angle brackets
+/ - slash, forward slash, whack
+\ - backslash, backwhack
+! - not, exclamation point, bang
+# - pound, hash, octothorp
+* - star, asterisk, splat
+|| - or operator, pipes
+&& - and operator
+$ - bling, cash money, dollar sign, jQuery
+NaN - Not a number, bread
+
+*/
+
+console.log('js');
